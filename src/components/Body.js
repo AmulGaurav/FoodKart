804847c5070f90113c592c727bed0143b8848ed1
Amul Gaurav <amulgaurav003@gmail.com>
2024-08-06 22:57:44 +0530

    Add main 'state' for the application

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,28 +1,62 @@
+import { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState([
+    {
+      data: {
+        id: "334475",
+        name: "KFC",
+        cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
+        costForTwo: 40000,
+        deliveryTime: 36,
+        avgRating: "3.8",
+      },
+    },
+
+    {
+      data: {
+        id: "334480",
+        name: "Dominos",
+        cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
+        costForTwo: 40000,
+        deliveryTime: 36,
+        avgRating: "4.5",
+      },
+    },
+    {
+      data: {
+        id: "334481",
+        name: "McDonalds",
+        cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
+        costForTwo: 40000,
+        deliveryTime: 36,
+        avgRating: "4.1",
+      },
+    },
+  ]);
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.data.avgRating >= 4
+            );
+
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+
       <div className="res-container">
-        <RestaurantCard
-          resName="Aggarwal Sweets"
-          cuisine="Biryani, North Indian, Asian"
-        />
-        <RestaurantCard resName="KFC" cuisine="Burger, Fast Food" />
-        <RestaurantCard resName="KFC" cuisine="Burger, Fast Food" />
-        <RestaurantCard resName="KFC" cuisine="Burger, Fast Food" />
-        <RestaurantCard resName="KFC" cuisine="Burger, Fast Food" />
-        <RestaurantCard resName="KFC" cuisine="Burger, Fast Food" />
-        <RestaurantCard resName="KFC" cuisine="Burger, Fast Food" />
-        <RestaurantCard resName="KFC" cuisine="Burger, Fast Food" />
-        <RestaurantCard resName="KFC" cuisine="Burger, Fast Food" />
-        <RestaurantCard resName="KFC" cuisine="Burger, Fast Food" />
-        <RestaurantCard resName="KFC" cuisine="Burger, Fast Food" />
-        <RestaurantCard resName="KFC" cuisine="Burger, Fast Food" />
-        <RestaurantCard resName="KFC" cuisine="Burger, Fast Food" />
-        <RestaurantCard resName="KFC" cuisine="Burger, Fast Food" />
-        <RestaurantCard resName="KFC" cuisine="Burger, Fast Food" />
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+        ))}
       </div>
     </div>
   );
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,4 +1,6 @@
-const RestaurantCard = ({ resName, cuisine }) => {
+const RestaurantCard = ({ resData }) => {
+  const { name, avgRating, cuisines, costForTwo, deliveryTime } = resData?.data;
+
   return (
     <div className="res-card">
       <img
@@ -6,10 +8,12 @@ const RestaurantCard = ({ resName, cuisine }) => {
         alt="res-logo"
         src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/5e904b0f43f2c46f0a3923985053b2a2"
       />
-      <h3>{resName}</h3>
-      <h4>{cuisine}</h4>
-      <h4>4.4 stars</h4>
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating} stars</h4>
       <h4>38 minutes</h4>
+      <h4>₹{costForTwo / 100} FOR TWO</h4>
+      <h4>{deliveryTime} minutes</h4>
     </div>
   );
 };
